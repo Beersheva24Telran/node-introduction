2c1eea1e3814c8fa6a40605f3209559853c0bcc7
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-03 11:31:16 +0200

    promises

--- index.mjs
+++ index.mjs
@@ -1,14 +1,8 @@
 
 import logger from './logger.mjs';
-import fs from 'node:fs'
-const data = fs.readFile('./file.txt','base64' ,(err, data) => {
-    if(err) {
-        logger.error(err)
-    } else {
-        console.log(data)
-    }
-});
+import {readFile, writeFile} from 'node:fs/promises'
+const data = await readFile('./file.txt','utf-16le')
 console.log(data)
-fs.writeFile('./file.txt', ["kuku", "kukureku", "Hello World!"].join('\n'), 'utf-16le',
- () => logger.debug('file saved in utf-16le format'))
+writeFile('./file.txt', ["kuku", "kukureku", "שלום"].join('\n'), 'utf-16le')
+logger.info("function finished");
 
