8b14c0258ab943a24d88898cf05c7bfdbdde4b4b
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-06 15:39:06 +0200

    http server introduction

--- index.mjs
+++ index.mjs
@@ -5,7 +5,11 @@ const port = process.env.PORT ?? 3500;
 server.listen(port);
 console.log (`listening on the port ${port} `)
 server.on('request', async (req, res) => {
-    console.log(req.method, req.url)
+    console.log(req.method, req.url);
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode=300;
+    res.write("just bad request");
+    res.end();
 })
 
 
