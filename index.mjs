b6867e1b8fa35f2a28224207d4d221b32c92714d
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-06 13:03:34 +0200

    http server creation

--- index.mjs
+++ index.mjs
@@ -1,14 +1,12 @@
 
-import stream from "./file-stream.mjs";
-import writableStream from "./file-writable-stream.mjs";
-(async () => {
-    for await(const res of stream) {
-        console.log(res.toString());
-    }
-})()
-writableStream.write("Hello\n ");
-writableStream.write("World !!!");
-writableStream.end();
+import http from 'node:http'
+const server = http.createServer();
+const port = process.env.PORT ?? 3500;
+server.listen(port);
+console.log (`listening on the port ${port} `)
+server.on('request', async (req, res) => {
+    console.log(req.method, req.url)
+})
 
 
 
