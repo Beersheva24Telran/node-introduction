78345f2dbbf000fbdb13c68b3897a171a2fd0445
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-06 11:35:20 +0200

    redable with events

--- index.mjs
+++ index.mjs
@@ -1,15 +1,8 @@
 
-import logger from './logger.mjs';
-import DistributionMessageLevels from './DistributionMessageLevels.mjs';
-import FilterLogMessages from './FIlterLogMessages.mjs';
-const filter1 = new FilterLogMessages(logger,["hello"], "debug");
-const filter2 = new FilterLogMessages(logger,["hello", "kuku"], "debug");
-const distribution = new DistributionMessageLevels(logger);
-logger.log("debug", "Hello world");
-logger.log("debug", "kuku");
-logger.log('info', "kukureku");
-console.log(distribution.getDistribution());
-console.log(filter2.getMessage());
+import stream from "./sender-data.mjs";
+let res = '';
+stream.on('data', chunk => res += chunk);
+stream.on('end', () => console.log(res));
 
 
 
---
+++ sender-data.mjs
@@ -0,0 +1,6 @@
+import Stream from 'node:stream'
+const stream = new Stream.Readable();
+stream.push("Hello ");
+stream.push("World");
+stream.push(null);
+export default stream;
